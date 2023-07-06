9e890ccab599e588613857c7f9fd5a9bc6a8fa11
Egor <egor.lysak13@gmail.com>
2023-07-06 13:12:57 +0100

    added a closure calculator

--- hw-7/index.js
+++ hw-7/index.js
@@ -1,108 +1,78 @@
-const questions = [
+const RIGHT_ANSWER = 10;
+const WRONG_ANSWER = 0;
+const QUIZ = [
     {
         question: 'Сколько хромосом у здорового человека?',
         answer: '46',
-        type: 'prompt',
+        type: prompt,
     },
     {
         question: 'Путин - хуйло',
         answer: true,
-        type: 'confirm',
+        type: confirm,
     },
     {
         question: 'Сколько хромосом у Путина?',
         answer: '47',
-        type: 'prompt',
+        type: prompt,
     },
     {
         question: 'Сколько тупых овец в московии (в млн)?',
         answer: '144',
-        type: 'prompt',
+        type: prompt,
     },
     {
         question: 'Снесли ли памятник Екатерине-2 в Одессе?',
         answer: true,
-        type: 'confirm',
+        type: confirm,
     },
     {
         question: 'Сколько черных пакетов выделяются на одного орка?',
         answer: '1',
-        type: 'prompt',
+        type: prompt,
     },
     {
         question: 'На сколько вы оцениваете работу ЗСУ от 1 до 10?',
         answer: '10',
-        type: 'prompt',
+        type: prompt,
     },
     {
         question: 'Со скольких позиций готовилось нападение на Беларусь?',
         answer: '4',
-        type: 'prompt',
+        type: prompt,
     },
     {
         question: 'Нужно ли сжигать сосийский флаг?',
         answer: true,
-        type: 'confirm',
+        type: confirm,
     },
     {
         question: 'Поддерживаете ли вы уход иностранных компаний из московии?',
         answer: true,
-        type: 'confirm',
+        type: confirm,
     },
     {
         question: 'Считаете ли вы сосию своим домом?',
         answer: false,
-        type: 'confirm',
+        type: confirm,
     },
 ];
 
     
-let score = 0;
+const score = runCheck(QUIZ);
 
-function askQuestion(question) {
-    let userAnswer;
+alert(score);
 
-    if (question.type === 'prompt') {
-        userAnswer = askPromptQuestion(question.question);
-    } else if (question.type === 'confirm') {
-        userAnswer = askConfirmQuestion(question.question);
-    }
-
-    if(isAnswerCorrect(userAnswer, question.answer)) {
-        score += 10;
-    }
-    
-    
-}
-
-function isAnswerCorrect(userAnswer, correctAnswer) {
-    if (typeof correctAnswer === 'boolean') {
-        return Boolean(userAnswer) === correctAnswer;
-    } else {
-        return userAnswer.toLowerCase() === correctAnswer.toLowerCase();
-    }
-}
-
-function runCheck() {
+function runCheck(questions) {
+    let score = WRONG_ANSWER;
 
     for (let i = 0; i < questions.length; i++) {
-        askQuestion(questions[i]);
+        const { type, question, answer} = questions[i];
+
+        if (type(question) === answer) {
+            score += RIGHT_ANSWER
+        }
     }
-     if (score >= 70 && score <= 110) {
-        alert(`Результат: ${score} очков. Поздравляю!`);
-    } else if (score >= 40 && score < 70) {
-        alert(`Результат: ${score} очков. Ты можешь лучше. Попробуй еще раз!`);
-    } else if(score < 40) {
-        alert(`Результат: ${score} очков. Геть з України!`);
-    }
-}
- runCheck();
 
-function askPromptQuestion(questionText) {
-    return prompt(questionText);  
+   return score;
 }
-
-function askConfirmQuestion(questionText) {
-    return confirm(questionText);
-}
-
---
+++ hw-8/index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const calculator = createCalculator(100);
+
+calculator.add(10);
+calculator.add(10);
+calculator.sub(20);
+calculator.set(20);
+calculator.add(10);
+calculator.add(10);
+calculator.add('qwe');
+console.log(calculator.get());
+calculator.reset();
+console.log(calculator.get());
+
+
+function createCalculator(baseValue) {
+    let result = baseValue;
+
+    function isValidNumber(num) {
+        return typeof num === 'number' && !isNaN(num);
+    }
+     
+    return {
+      add: (num) => {
+        if (isValidNumber(num)) {
+          result += num;  
+        }
+      },
+      sub: (num) => {
+        if (isValidNumber(num)) {
+          result -= num;
+        }
+      },
+      reset: () => {
+        result = baseValue;
+        console.log(`Значення скинуто до базового значення: ${baseValue}`);
+      },
+      set: (newValue) => {
+        if (isValidNumber(newValue)) {
+            result = newValue;
+        }
+      },
+      get: () => {
+        return result;
+      }
+    };
+  }
+  
