fcb51a6f330ca53d42edf3d0c270a5fa470f1bf5
Yehor Lysak <egor.lysak13@gmail.com>
2023-09-21 15:05:35 +0100

    hw-27/todos

--- lib-api/url.js
+++ lib-api/url.js
@@ -2,3 +2,4 @@ export const todoUrl = 'https://mock-api-5678.nw.r.appspot.com/todos/';
 export const contactsUrl = 'https://mock-api-5678.nw.r.appspot.com/contacts/';
 export const contactsUrl2 = 'https://62054479161670001741b708.mockapi.io/api/contacts/';
 export const waitersUrl = 'http://localhost:4000/waiters/';
+export const todosUrl = 'http://localhost:4000/todos/';
